bacbc86ec06c6473f45f2ba9675e3c050fac613c
nagappan <nagappan08@gmail.com>
2023-12-26 13:40:31 +0530

    added logic to use the add to cart component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import { LightBox } from "react-lightbox-pro";
+import { AddToCartComponent }  from "addtocart-react-component";
+import React, { useState, useRef } from "react";
 
 function App() {
+  const [toggler, setToggler] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(4);
+  const [showval, setShowval] = useState("");
+  const cartCountRef = useRef();
+  const cartProps = {
+    title: "AddToCart",
+    maxVal: 10,
+    minVal: 0,
+    initialVal: 2,
+    ref: cartCountRef,
+  };
+  const data = [
+    {
+      media: "https://picsum.photos/200/300",
+      type: "IMAGE",
+      caption: "React Image",
+    },
+    // without caption
+    {
+      media: "https://picsum.photos/200/300",
+      type: "IMAGE",
+    },
+    {
+      media: "https://picsum.photos/200/300",
+      type: "IMAGE",
+    },
+    {
+      media: "http://techslides.com/demos/sample-videos/small.mp4",
+      type: "VIDEO",
+      caption: "Sample Video with Caption",
+    },
+    {
+      media: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+      type: "AUDIO",
+    },
+  ];
+  
+  const checkVal = (e) => {
+    e.preventDefault();
+    console.log("check val", cartCountRef.current.outerText, cartCountRef);
+    setShowval(cartCountRef.current.outerText);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setToggler(true);
+        }}
+      >
+        Click to Open LightBox
+      </button>
+      
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <h1>Add To Cart Component</h1>
+      <AddToCartComponent cartBtnProps={cartProps}/>
+      <h6>We can check Cart Value outside the component</h6>
+      <button onClick={checkVal}>Check Value</button>
+      <p>{showval}</p>
+      <LightBox currentSlide={currentSlide} mediaItems={data} toggler={toggler} togglerFunc={setToggler} callback={() => {
+              console.log("going to do a callback");
+              setToggler(false);
+            }}/>
     </div>
   );
 }
